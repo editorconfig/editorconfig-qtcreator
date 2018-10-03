656f84a4b837fb11021856c5fa841aa069803c1d
Herbert Graeber <herbert@graeber-clan.de>
2018-10-03 14:15:36 +0200

    Add german translations

--- editorconfig_de.ts
+++ editorconfig_de.ts
@@ -1,4 +1,90 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
+<context>
+    <name>EditorConfig</name>
+    <message>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+</context>
+<context>
+    <name>EditorConfig::EditorConfigData</name>
+    <message>
+        <source>override tab width with %1</source>
+        <translation>Überschreibe den Tabulatorabstand mit %1</translation>
+    </message>
+    <message>
+        <source>override indent size with %1</source>
+        <translation>Überschreibe die Einrückung mit %1</translation>
+    </message>
+    <message>
+        <source>override indent style with &apos;tab&apos;</source>
+        <translation>Überschreibe den Einrückungsstil mit &apos;tab&apos;</translation>
+    </message>
+    <message>
+        <source>override indent style with &apos;space&apos;</source>
+        <translation>Überschreibe den Einrückungsstil mit &apos;space&apos;</translation>
+    </message>
+    <message>
+        <source>override add final newline with &apos;true&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>override add final newline with &apos;false&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>override trim trailing whitespace with &apos;true&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>override trim trailing whitespace with &apos;false&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>override charset with &apos;%2&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditorConfig::EditorConfigDialog</name>
+    <message>
+        <source>Editorconfig File</source>
+        <translation>Editorconfig-Datei</translation>
+    </message>
+</context>
+<context>
+    <name>EditorConfig::EditorConfigPage</name>
+    <message>
+        <source>Editorconfig Details</source>
+        <translation>Editorconfig Details</translation>
+    </message>
+</context>
+<context>
+    <name>EditorConfig::EditorConfigWizard</name>
+    <message>
+        <source>Editorconfig File</source>
+        <translation>Editorconfig-Datei</translation>
+    </message>
+    <message>
+        <source>Creates an initial editorconfig file derived from the current C++, Qml and generic files settings.</source>
+        <translation>Erzeuge eine neue Editorconfig-Datei auf der Basis der aktuellen Qt-Creator-Einstellungen.</translation>
+    </message>
+</context>
+<context>
+    <name>EditorConfigPage</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Pfad</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Path:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
